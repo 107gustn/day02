797e14d3822369c006d23baf328bd68a0d80212d
khs <khs@k.k>
2022-05-16 17:21:56 +0900

    입력받는 문법

--- src/day02/Quiz01.java
+++ src/day02/Quiz01.java
@@ -7,29 +7,30 @@ public class Quiz01 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		String name;
+		int kScore, mScore, EScore;
+		
 		System.out.print("당신의 이름은 무엇입니까? ");
-		String name = input.next();
+		name = input.next();
 		
 		System.out.print(name + "님의 국어 점수 : ");
-		int gscore = input.nextInt();
+		kScore = input.nextInt();
 		
 		System.out.print(name + "님의 영어 점수 : ");
-		int escore = input.nextInt();
+		EScore = input.nextInt();
 		
 		System.out.print(name + "님의 수학 점수 : ");
-		int mscore = input.nextInt();
-		
-		int sum = gscore + escore + mscore;
+		mScore = input.nextInt();
 		
 		System.out.println();
 		System.out.println("=============");
 		System.out.println("이 름 : " + name);
 		System.out.println("=============");
-		System.out.println("국 어 : " + gscore);
-		System.out.println("영 어 : " + escore);
-		System.out.println("수 학 : " + mscore);
+		System.out.println("국 어 : " + kScore);
+		System.out.println("영 어 : " + EScore);
+		System.out.println("수 학 : " + mScore);
 		System.out.println("=============");
-		System.out.println("합 계 : " + sum);
+		System.out.println("합 계 : " + (kScore + EScore + mScore));
 		System.out.println("=============");
 
 	}
